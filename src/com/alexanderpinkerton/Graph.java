15dfd6b29ae01c0aba9d5b8846e0788ee5dc831d
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-04-16 14:09:13 -0400

    First Commit

---
+++ src/com/alexanderpinkerton/Graph.java
@@ -0,0 +1,199 @@
+package com.alexanderpinkerton;
+
+import java.util.*;
+
+/**
+ * Created by Ace on 4/15/15.
+ */
+
+
+
+public class Graph<T> {
+
+
+    private int edgeCount=0;
+    private int vertexCount=0;
+    private static final int INFINITY = Integer.MAX_VALUE;
+
+    private Map<String,Vertex> vertexList = new HashMap<String,Vertex>( );
+
+
+
+
+    public Vertex addVertex(String name){
+        if(!vertexList.containsKey(name)){
+            Vertex v = new Vertex(name);
+            vertexList.put(name,v);
+            vertexCount++;
+            return v;
+        }
+        //System.out.println("Vertex not added.");
+        return null;
+    }
+
+    public Vertex addVertex(String name, T data){
+        if(!vertexList.containsKey(name)){
+            Vertex v = new Vertex(name, data);
+            vertexList.put(name,v);
+            vertexCount++;
+            return v;
+        }
+        //System.out.println("Vertex not added.");
+        return null;
+    }
+
+
+
+    public void addEdge(Vertex v1, Vertex v2){
+        v1.outgoingEdges.add(new Edge(v1,v2));
+        edgeCount++;
+    }
+
+    public void addEdge(Vertex v1, Vertex v2, float weight){
+        v1.outgoingEdges.add(new Edge(v1,v2,weight));
+        edgeCount++;
+    }
+
+
+    public int getEdgeCount() {
+        return edgeCount;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+    public Map<String, Vertex> getVertexList() {
+        return vertexList;
+    }
+
+    public void setVertexList(Map<String, Vertex> vertexList) {
+        this.vertexList = vertexList;
+    }
+
+
+
+
+
+
+
+
+
+    //=======================================================================================================
+
+
+    private class Vertex<V> {
+        private String name;
+        private Object data;
+        private List<Edge> outgoingEdges;
+
+        private Vertex(String name){
+            outgoingEdges = new LinkedList<Edge>();
+            this.name = name;
+        }
+
+        private Vertex(String name, V data){
+            outgoingEdges = new LinkedList<Edge>();
+            this.data = data;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+
+        public List<Edge> getOutgoingEdges() {
+            return outgoingEdges;
+        }
+
+        public void setOutgoingEdges(List<Edge> outgoingEdges) {
+            this.outgoingEdges = outgoingEdges;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Vertex<?> vertex = (Vertex<?>) o;
+
+            if (name != null ? !name.equals(vertex.name) : vertex.name != null) return false;
+            if (data != null ? !data.equals(vertex.data) : vertex.data != null) return false;
+            if (outgoingEdges != null ? !outgoingEdges.equals(vertex.outgoingEdges) : vertex.outgoingEdges != null)
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (data != null ? data.hashCode() : 0);
+            result = 31 * result + (outgoingEdges != null ? outgoingEdges.hashCode() : 0);
+            return result;
+        }
+    }
+
+    //=======================================================================================================
+
+
+    private class Edge {
+
+        private Vertex start;
+        private Vertex end;
+        private float weight;
+
+        private Edge(Vertex start, Vertex end){
+            this.start = start;
+            this.end = end;
+        }
+
+        private Edge(Vertex start, Vertex end, float weight){
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        public Vertex getStart() {
+            return start;
+        }
+
+        public void setStart(Vertex start) {
+            this.start = start;
+        }
+
+        public Vertex getEnd() {
+            return end;
+        }
+
+        public void setEnd(Vertex end) {
+            this.end = end;
+        }
+
+        public float getWeight() {
+            return weight;
+        }
+
+        public void setWeight(float weight) {
+            this.weight = weight;
+        }
+
+    }
+
+    //=======================================================================================================
+
+
+
+}
---
+++ src/com/alexanderpinkerton/Main.java
@@ -0,0 +1,50 @@
+package com.alexanderpinkerton;
+
+import sun.security.provider.certpath.Vertex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Graph graph = new Graph();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                // process the line.
+                String[] lineContents = line.split(" ");
+
+                graph.addVertex(lineContents[0]);
+                graph.addVertex(lineContents[1]);
+
+                /*
+                graph.addEdge(
+                        graph.addVertex(lineContents[0]),
+                        graph.addVertex(lineContents[1]),
+                        Float.parseFloat(lineContents[2]));
+                */
+
+
+            }
+        }catch (IOException e){
+
+        }
+
+
+        Iterator it = graph.getVertexList().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+
+
+
+    }
+}
